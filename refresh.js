a1b2ac10e7dfce9a6f16663392e27b4099847126
Kinn Coelho Julião <kinncj@gmail.com>
2013-07-31 20:20:46 -0400

    Added command line support

--- refresh.js
+++ refresh.js
@@ -3,7 +3,7 @@
 
 var program = require('commander');
 var server  = require('./lib/server');
-var usage   = '-s <localhost> -p <1337> -r <localhost> -t<80> -d </var/www/kinncj>';
+var usage   = '-s <localhost> -p <1337> -r <localhost> -t<80> -d </var/www/kinncj> -c ["ls -la"]';
 program
   .version('0.0.1')
   .usage(usage)
@@ -12,6 +12,7 @@ program
   .option('-r, --remoteserver <remoteserver> -no-', 'Remote Server', 'localhost')
   .option('-t, --remoteport <remoteport> -no-', 'Remote server port', '80')
   .option('-d, --directory <directory> -no-', 'Directory to watch', '/tmp')
+  .option('-c, --command [command] -no-', 'A command to run for each update', 'ls -ls')
   .parse(process.argv);
  
 function check(program)
@@ -22,18 +23,20 @@ function check(program)
 		case program.server:
 		case program.remoteport:
 		case program.directory:
-			console.log('Usage: refresh '+usage);
+			console.log('Usage: refresh ' + usage);
 			return false;
 		break;
 	}
+
 	return true;
 }
 
 if (!check(program)) {
 	return;
 }
+
 try{
-server.create(program.server, program.localport, program.remoteserver, program.remoteport, program.directory);
+	server.create(program.server, program.localport, program.remoteserver, program.remoteport, program.directory, program.command);
 }catch(err){
- console.log(err);
+	console.log(err);
 }
